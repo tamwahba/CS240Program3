6d6cc922894fbd8e27fed193f91d6aaa17dfc67c
Tamer Wahba <tamwahba@hotmail.com>
2014-11-24 17:25:56 -0500

    added method signatures for date class

--- Date.h
+++ Date.h
@@ -1,4 +1,21 @@
 #ifndef DATE
 #define DATE
 
+class Date
+{
+public:
+	Date(int, int, int);
+	
+	int day();
+	int month();
+	int year();
+	
+	Date operator+(const &Date) const;
+	Date operator-(const &Date) const;
+	bool operator==(const &Date) const;
+	
+	friend ostream& operator<<(ostream&, const Date&);
+	
+};
+
 #endif
