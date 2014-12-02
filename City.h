292d79db38f18e3d2ba4e85800c642f15dd9c85a
Tamer Wahba <twahba1@cs.binghamton.edu>
2014-12-02 15:28:21 -0500

    added header for city class

--- City.h
+++ City.h
@@ -1,4 +1,23 @@
 #ifndef CITY
 #define CITY
 
+#include <string>
+#include <list>
+using namespace std;
+
+class City;
+
+#include "Flight.h"
+
+class City
+{
+	public:
+		City();
+
+	private:
+		string name;
+		list<Flight> outbound;
+	
+};
+
 #endif
