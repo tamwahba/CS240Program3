5b521cc0234972be65aab0d4ce5000c3db8910af
Tamer Wahba <twahba1@cs.binghamton.edu>
2014-12-02 16:24:53 -0500

    added header info for TripAdvisor class

--- TripAdvisor.h
+++ TripAdvisor.h
@@ -1,4 +1,33 @@
 #ifndef TRIPADVISOR
 #define TRIPADVISOR
 
+#include <list>
+#include <string>
+using namespace std;
+
+#include "Flight.h"
+#include "City.h"
+#include "Time.h"
+#include "Date.h"
+
+class TripAdvisor
+{
+	public:
+		TripAdvisor();
+		// creates new city and adds it to list of citys if new, 
+		//  then adds flight to the list of outbound flights
+		//  from departure city.
+		void addFlight(sting);
+		// return ordered list of flights. take in origin name, destination
+		list<Flight> getMeThere(string, string);
+		list<Flight> fewestHops(string, string);
+		list<Flight> shortestTrip(string, string);
+
+
+	private:
+		list<City*> cities;
+
+	
+};
+
 #endif
