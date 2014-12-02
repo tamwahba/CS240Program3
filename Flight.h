bb9598e11434fc2340d34579ea18adfd5b5cc231
Tamer Wahba <twahba1@cs.binghamton.edu>
2014-12-02 15:29:35 -0500

    dded header file for flight

--- Flight.h
+++ Flight.h
@@ -1,4 +1,24 @@
 #ifndef FLIGHT
 #define FLIGHT
 
+#include <string>
+using namespace std;
+
+class Flight;
+
+#include "City.h"
+
+class Flight
+{
+	public:
+		Flight();
+
+	private:
+		int cost;
+		Time depart;
+		Time duration;
+		City* destination;
+	
+};
+
 #endif
