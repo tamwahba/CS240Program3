6c8bb8fece08f05816c8befcbdcea0bf8b908247
Tamer Wahba <tamwahba@hotmail.com>
2014-12-05 14:21:10 -0500

    Moved CLI from test to main

--- main.cpp
+++ main.cpp
@@ -9,5 +9,53 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-		return 0;
+	string selection;
+	TripAdvisor t;
+	if (argc == 2) {
+		ifstream in (argv[1]);
+		string line;
+		if (in.is_open() )
+		{
+			while (!in.eof() )
+			{
+				getline (in, line);
+				t.addFlight(line);
+			}
+		} else {
+			cout << "Error opening file " <<endl;
+		}
+		while (true)
+		{
+			string c; //char c?
+			cout << "Please select one of the following options" << endl;
+			cout <<"\t J: Just get me there" << endl;
+			cout << "\t F: Fewest Hops" << endl;
+			cout << "\t S: Shortest Trip" << endl;
+			cout << "\t C: Cheapest" << endl;
+			cout << "or press E to end" << endl;
+			
+			cin >> c;
+			
+			if (c== "J" || c== "j") {
+				cout << "selected J" << endl;
+				//t.JustGetMeThere();
+			} else if (c == "F" || c== "f") {
+				cout << "selected F" << endl;
+				//t.FewestHops();
+			} else if (c == "S" || c== "s") {
+				cout << "selected S" << endl;
+				//t.ShortestTrip();
+			} else if (c == "C" || c== "c") {
+				cout << "selected C" << endl;
+				//t.Cheapest()
+			} else if (c == "E" ) {
+				break;
+			} else {
+				cout << "Error please enter J, F,C, or S." << endl;
+			}
+		}
+	} else {
+		cout << "Cannot run program without DDFS file as argument."
+	}
+	return 0;
 }
--- test.cpp
+++ test.cpp
@@ -59,56 +59,6 @@ void TripAdvisorTest() {
 
 int main(int argc, char const *argv[])
 {
-	string selection;
-	TripAdvisor t;
-	if (argc == 2) {
-		ifstream in (argv[1]);
-		string line;
-		if (in.is_open() )
-		{
-			while (!in.eof() )
-			{
-				getline (in, line);
-				t.addFlight(line);
-			}
-		}
-		if (!in.is_open() )
-		{
-			cout << "Error opening file " <<endl;
-		}
-	}
-		while (true)
-		{
-			string c; //char c?
-			cout << "Please select one of the following options" << endl;
-			cout <<"\t J: Just get me there" << endl;
-			cout << "\t F: Fewest Hops" << endl;
-			cout << "\t S: Shortest Trip" << endl;
-			cout << "\t C: Cheapest" << endl;
-			cout << "or press e to end" << endl;
-			cin >> c;
-			
-			if (c== "J" || c== "j") {
-                cout << "pressed J" << endl;
-            }
-				//t.JustGetMeThere();
-            else if (c == "F" || c== "f") {
-                cout << "selected F" << endl;
-            }
-					//t.FewestHops();
-            else if (c == "S" || c== "s") {
-                cout << "selected S" << endl;
-            }
-						//t.ShortestTrip();
-            else if (c == "C" || c== "c") {
-                cout << "selected C" << endl;
-            }
-							//t.Cheapest()
-            else if (c == "e" ) {break;}
-			else {
-				cout << "Error please enter J, F,C, or S" << endl;
-			}
-		}
 	TimeTest();
 	DateTest();
     TripAdvisorTest();
