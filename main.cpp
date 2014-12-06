5949579d7411b3be607e4382f9a5b2bbf578f2a9
Tamer Wahba <twahba1@cs.binghamton.edu>
2014-12-06 18:29:55 -0500

    Added user error handling, fixed file input loop

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <limits>
 using namespace std;
 
 #include "Time.h"
@@ -8,37 +10,113 @@ using namespace std;
 #include "Flight.h"
 #include "TripAdvisor.h"
 
-struct searchParams {
-    City* departCity;
-    City* arriveCity;
-    Date departDate;
-    Time departTime;
-    Date arriveDate;
-    Time arriveTime;
-};
-
 // returns data for search converted from input strings to proper types
 // error checking occurs here, future functions could assume all data 
 // returned to be valid. if user chooses to quit, returns null.
-searchParams getConstraints(TripAdvisor& t) {
-    string departName, arriveName, departDate, departTime;
-    string returnDate, returnTime;
-    searchParams p;
-    cout << "Enter departure city: ";
-    getline(cin ,departName);
+bool getConstraints(TripAdvisor& t, searchParams& p) {
+    t.printCitiesTo(cout);
+    string departName, arriveName;
+    City* departCity = nullptr;
+    City* returnCity = nullptr;
+    Time departTime, returnTime;
+    Date departDate, returnDate;
+
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    while (cin.good()) {
+        cout << "Enter departure city: ";
+        getline(cin, departName);
+        cout << "you entered: " << departName << endl; // temporary for testing
+        departCity = t.getCity(departName);
+        if (departCity != nullptr) {
+            break;
+        } else {
+            if (cin.eof()) {
+                cout << "break" << endl;
+                break;
+            }
+            cout << "City not in schedule. Please try again." << endl;
+            //cin.clear();
+            //cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+
     // convert to city* by searching in t
     // then p.departCity = result. or keep looping
-    cout << "Enter arrival city: ";
-    getline(cin, arriveName);
-    cout << "Enter departure date (mm/dd/yyy): ";
-    getline(cin, departDate);
-    cout << "Enter departure time (hh:mm): ";
-    getline(cin, departTime);
-    cout << "Enter return date (mm/dd/yyy): ";
-    getline(cin, returnDate);
-    cout << "Enter return time (hh:mm): ";
-    getline(cin, returnTime);
-    return p;
+    while (cin.good()) {
+        cout << "Enter arrival city: ";
+        getline(cin, arriveName);
+        cout << "you entered: " << arriveName << endl;
+        returnCity = t.getCity(arriveName);
+        if (returnCity != nullptr) {
+            break;
+        } else {
+            if (cin.eof())
+                break;
+            cout << "City not in schedule. Please try again." << endl;
+            //cin.clear();
+            //cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+
+    while (cin.good()) {
+        cout << "Enter departure date (mm/dd/yyyy): ";
+        if (cin >> departDate) {
+            break;
+        } else {
+            if (cin.eof())
+                break;
+            cout << "Invalid date. Please try again." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+    
+    while (cin.good()) {
+        cout << "Enter departure time (hh:mm): ";
+        if( cin >> departTime) {
+            break;
+        } else {
+            if (cin.eof())
+                break;
+            cout << "Invalid time. Please try again." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+
+    
+    while (cin.good()) {
+        cout << "Enter return date (mm/dd/yyyy): ";
+        if (cin >> returnDate) {
+            break;
+        } else {
+            if (cin.eof())
+                break;
+            cout << "Invalid date. Please try again." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+    
+    while (cin.good()) {
+        cout << "Enter return time (hh:mm): ";
+        if (cin >> returnTime) {
+            break;
+        } else {
+            if (cin.eof())
+                break;
+            cout << "Invalid time. Please try again." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+
+    if (!cin.good())
+        return false;
+
+    return true;
 }
 
 int main(int argc, char const *argv[])
@@ -50,15 +128,16 @@ int main(int argc, char const *argv[])
 		string line;
 		if (in.is_open() )
 		{
-			while (!in.eof() )
-			{
-				getline (in, line);
-				t.addFlight(line);
-			}
+            while (in.good()) {
+                getline(in, line, '\n');
+                if (line != "") {
+                    t.addFlight(line);
+                }
+            }
 		} else {
 			cout << "Error opening file " <<endl;
 		}
-		while (true)
+		while (cin.good())
 		{
 			string c; //char c?
 			cout << "Please select one of the following options" << endl;
@@ -68,25 +147,28 @@ int main(int argc, char const *argv[])
 			cout << "\t C: Cheapest" << endl;
 			cout << "or press E to end" << endl;
 			
+            searchParams p;
+
 			cin >> c;
-			
-			if (c== "J" || c== "j") {
+            if (c == "J" || c == "j") {
 				cout << "selected J" << endl;
-				//t.JustGetMeThere();
-			} else if (c == "F" || c== "f") {
+				if (getConstraints(t, p))
+                    list<Flight> itinerary = t.getMeThere(p);
+			} else if (c == "F" || c == "f") {
 				cout << "selected F" << endl;
-				//t.FewestHops();
-			} else if (c == "S" || c== "s") {
+				//t.fewestHops(" ", " ");
+			} else if (c == "S" || c == "s") {
 				cout << "selected S" << endl;
-				//t.ShortestTrip();
-			} else if (c == "C" || c== "c") {
-				cout << "selected C" << endl;
-				//t.Cheapest()
+				//t.shortestTrip(" ", " ");
+			} else if (c == "C" || c == "c") {
+                //t.cheapest(" ", " ");
 			} else if (c == "E" ) {
 				break;
 			} else {
 				cout << "Error please enter J, F,C, or S." << endl;
 			}
+            cin.clear();
+            //cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		}
 	} else {
 		cout << "Cannot run program without DDFS file as argument." << endl;
